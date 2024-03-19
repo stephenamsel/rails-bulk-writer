0a54fb9bbd487724c051311f83d947bc18a42efc
stephenamsel <amselstephen@gmail.com>
2024-03-19 10:37:37 -0400

    reset deletion-ids

--- lib/rails_bulk_writer/lib/delete_marker.rb
+++ lib/rails_bulk_writer/lib/delete_marker.rb
@@ -6,12 +6,5 @@ module RailsBulkWriter
         resets { to_delete = {} }
     end
 
-    class CacheConfig < ActiveSupport::CurrentAttributes
-        # using CurrentAttributes to keep it specific to sequential requests
-        # With current Rails, this is equivalent to a sub-hash within the Thread.current
 
-        attribute :cache_db_name
-
-        resets {} # do not erase this data upon reset
-    end
 end
